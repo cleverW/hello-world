8353d3937110519decb27def61abe752df6a7e6b
cleverW <931142193@qq.com>
2020-04-15 21:20:05 +0800

    三目运算的规则

---
+++ 三目运算符.cpp
@@ -0,0 +1,12 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a , b ,c;
+//	cin>>a>>b;
+//	a > b ? c = a : c = b;
+//	cout<<"C的结果是！"<<c<<endl;
+//	system("pause");
+//	return 0;
+//}
