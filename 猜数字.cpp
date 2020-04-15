fbf2e947753c87869cc2bf95177a4efa2fb89f63
cleverW <931142193@qq.com>
2020-04-15 21:12:18 +0800

    猜100之内数字的小游戏

---
+++ 猜数字.cpp
@@ -0,0 +1,33 @@
+//#include<iostream>
+//#include<ctime>
+//using namespace std;
+//
+//int main()
+//{
+//	//int a = rand()%100 + 1;
+//	srand((unsigned)time(NULL));
+//	int a = rand()%100 + 1;
+//	int b ;
+//	cout<<"猜数字开始了！！"<<endl;
+//	cout<< "请输入您猜的数字！（1 - 100）: ";
+//	while(1)
+//	{
+//		
+//		cin >> b;
+//		if(a > b)
+//		{
+//			cout<<"您输入的数字过小请重新输入！";
+//		}else if(a < b)
+//		{
+//			cout<<"您输入的数字过大请重新输入！";
+//		}else
+//		{
+//			cout<<"您输入的数字正确，所猜的数字就是："<< a<<endl;
+//			break;
+//		}
+//	}
+//	
+//	
+//	system("pause");
+//	return 0;
+//}
