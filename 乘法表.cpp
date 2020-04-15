542a12ec28a402df99194bdf3f4c0144a42e6aae
cleverW <931142193@qq.com>
2020-04-15 21:18:55 +0800

    乘法表

---
+++ 乘法表.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	for(int i = 1 ; i < 10 ; i++)
+	{
+		for(int j = 1 ; j <= i ; j++)
+		{
+			//if()
+			cout<<i<<"*"<<j<<"="<<i*j<<"  ";
+		
+		}
+		cout<<endl;
+	
+	}
+	system("pause");
+	return 0;
+}
