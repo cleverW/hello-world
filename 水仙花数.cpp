81c7256310355a07a9fcefc626b847af81b28f01
cleverW <931142193@qq.com>
2020-04-15 21:21:00 +0800

    三位数的水仙花数！！

---
+++ 水仙花数.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int a = 100;
+//	int b,c ,d;
+//
+//	do
+//	{
+//		b = a/100;
+//		c = (a%100)/10;
+//		d = (a%100)%10;
+//		if(b*b*b+c*c*c+d*d*d == a)
+//		{
+//			cout<<a<<endl;
+//		}
+//		a++;
+//	}while(a < 1000);
+//
+//	system("pause");
+//	return 0;
+//}
