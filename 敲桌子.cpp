c3a2b84058f2e36eb1f67c6c0242536fdb301afa
cleverW <931142193@qq.com>
2020-04-15 21:15:28 +0800

    敲七

---
+++ 敲桌子.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//using namespace std;
+//
+//
+//int main()
+//{
+//	int a = 1;
+//	for(;a < 100;a++)
+//	{
+//		if((a%7 == 0)||(a%10 == 7)|| (a/10 == 7 ))
+//		{
+//			cout<<"ÇÃ×À×Ó"<<a<<endl;
+//		}else
+//		{
+//			cout<<a<<endl;
+//		}
+//	
+//	}
+//	system("pause");
+//	return 0;
+//}
