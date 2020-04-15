6aef06a1358d059a101615d8020f051537929e54
cleverW <931142193@qq.com>
2020-04-15 21:17:21 +0800

    小猪比重量

---
+++ 三只小猪.cpp
@@ -0,0 +1,42 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int a , b , c;
+//	cout<<"请输入A小猪的体重： ";
+//	cin>>a;
+//	cout<<"请输入B小猪的体重： ";
+//	cin>>b;
+//	cout<<"请输入C小猪的体重： ";
+//	cin>>c;
+//
+//	cout<<"A小猪的体重："<<a<<endl;
+//	cout<<"B小猪的体重："<<b<<endl;
+//	cout<<"C小猪的体重："<<c<<endl;
+//
+//	if(a > b)
+//	{
+//		if(a > c)
+//		{
+//			cout<<"A小猪的体重最重！"<<endl;
+//		}else
+//		{
+//			cout<<"C小猪的体重最重！"<<endl;
+//		}
+//	
+//	}else
+//	{
+//		if(b > c)
+//		{
+//			cout<<"B小猪的体重最重！"<<endl;
+//		}else
+//		{
+//			cout<<"C小猪的体重最重！"<<endl;
+//		}
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
